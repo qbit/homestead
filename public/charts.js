316e6d45bdcb065b468d2b2c3e4ca7f0c3c8d4fe
Aaron Bieber <aaron@bolddaemon.com>
2016-09-23 08:36:57 -0600

    enable other graph

--- public/charts.js
+++ public/charts.js
@@ -154,133 +154,117 @@ $(function () {
 	    }
 	}
     });
-/*    
-		$('#gauge_GreenHouse').highcharts(
-		    makeSpeed("GreenHouse", 0, 120, 6),
-		    function (chart) {
-			if (!chart.renderer.forExport) {
-			    setInterval(function () {
-				$.get({
-				    url:'data/current/GreenHouse',
-				    success: function(data) {
-					var point = chart.series[0].points[0];
-					point.update(Math.floor(data.temp));
-				    }});
-			    }, 10000);
-			}
-		    });
-		*/
-	    });
-	/*
-  $(function () {
-  $('#linedata').highcharts({
-  chart: {
-  type: 'spline'
-  },
-  title: {
-  text: 'Weather data for Greenhouse'
-  },
-  subtitle: {
-  text: 'May 31 and and June 1, 2015 at two locations in Vik i Sogn, Norway'
-  },
-  xAxis: {
-  type: 'datetime',
-  labels: {
-  overflow: 'justify'
-  }
-  },
-  yAxis: {
-  title: {
-  text: 'Ideal Conditions'
-  },
-  minorGridLineWidth: 0,
-  gridLineWidth: 0,
-  alternateGridColor: null,
-  plotBands: [{ // Light air
-  from: 0.3,
-  to: 1.5,
-  color: 'rgba(68, 170, 213, 0.1)',
-  label: {
-  text: 'Too Cold',
-  style: {
-  color: '#606060'
-  }
-  }
-  }, { // Light breeze
-  from: 1.5,
-  to: 3.3,
-  color: 'rgba(0, 0, 0, 0)',
-  label: {
-  text: 'Cold',
-  style: {
-  color: '#606060'
-  }
-  }
-  }, { // Gentle breeze
-  from: 3.3,
-  to: 5.5,
-  color: 'rgba(68, 170, 213, 0.1)',
-  label: {
-  text: 'Just Right',
-  style: {
-  color: '#606060'
-  }
-  }
-  }, { // Moderate breeze
-  from: 5.5,
-  to: 8,
-  color: 'rgba(0, 0, 0, 0)',
-  label: {
-  text: 'Hot',
-  style: {
-  color: '#606060'
-  }
-  }
-  }, { // Fresh breeze
-  from: 8,
-  to: 11,
-  color: 'rgba(68, 170, 213, 0.1)',
-  label: {
-  text: 'Too Hot',
-  style: {
-  color: '#606060'
-  }
-  }
-  }]
-  },
-  tooltip: {
-  valueSuffix: ' °F'
-  },
-  plotOptions: {
-  spline: {
-  lineWidth: 4,
-  states: {
-  hover: {
-  lineWidth: 5
-  }
-  },
-  marker: {
-  enabled: false
-  },
-  pointInterval: 3600000, // one hour
-  pointStart: Date.UTC(2015, 4, 31, 0, 0, 0)
-  }
-  },
-  series: [{
-  name: 'Temperature',
-  data: [0.2, 0.8, 0.8, 0.8, 1, 1.3, 1.5, 2.9, 1.9, 2.6, 1.6, 3, 4, 3.6, 4.5, 4.2, 4.5, 4.5, 4, 3.1, 2.7, 4, 2.7, 2.3, 2.3, 4.1, 7.7, 7.1, 5.6, 6.1, 5.8, 8.6, 7.2, 9, 10.9, 11.5, 11.6, 11.1, 12, 12.3, 10.7, 9.4, 9.8, 9.6, 9.8, 9.5, 8.5, 7.4, 7.6]
-
-  }, {
-  name: 'Humidity',
-  data: [0, 0, 0.6, 0.9, 0.8, 0.2, 0, 0, 0, 0.1, 0.6, 0.7, 0.8, 0.6, 0.2, 0, 0.1, 0.3, 0.3, 0, 0.1, 0, 0, 0, 0.2, 0.1, 0, 0.3, 0, 0.1, 0.2, 0.1, 0.3, 0.3, 0, 3.1, 3.1, 2.5, 1.5, 1.9, 2.1, 1, 2.3, 1.9, 1.2, 0.7, 1.3, 0.4, 0.3]
-  }],
-  navigation: {
-  menuItemStyle: {
-  fontSize: '10px'
-  }
-  }
-  });
-  });
+});
 
 
-*/
+$(function () {
+    $('#linedata').highcharts({
+	chart: {
+	    type: 'spline'
+	},
+	title: {
+	    text: 'Weather data for Greenhouse'
+	},
+	subtitle: {
+	    text: 'May 31 and and June 1, 2015 at two locations in Vik i Sogn, Norway'
+	},
+	xAxis: {
+	    type: 'datetime',
+	    labels: {
+		overflow: 'justify'
+	    }
+	},
+	yAxis: {
+	    title: {
+		text: 'Ideal Conditions'
+	    },
+	    minorGridLineWidth: 0,
+	    gridLineWidth: 0,
+	    alternateGridColor: null,
+	    plotBands: [{ // Light air
+		from: 0.3,
+		to: 1.5,
+		color: 'rgba(68, 170, 213, 0.1)',
+		label: {
+		    text: 'Too Cold',
+		    style: {
+			color: '#606060'
+		    }
+		}
+	    }, { // Light breeze
+		from: 1.5,
+		to: 3.3,
+		color: 'rgba(0, 0, 0, 0)',
+		label: {
+		    text: 'Cold',
+		    style: {
+			color: '#606060'
+		    }
+		}
+	    }, { // Gentle breeze
+		from: 3.3,
+		to: 5.5,
+		color: 'rgba(68, 170, 213, 0.1)',
+		label: {
+		    text: 'Just Right',
+		    style: {
+			color: '#606060'
+		    }
+		}
+	    }, { // Moderate breeze
+		from: 5.5,
+		to: 8,
+		color: 'rgba(0, 0, 0, 0)',
+		label: {
+		    text: 'Hot',
+		    style: {
+			color: '#606060'
+		    }
+		}
+	    }, { // Fresh breeze
+		from: 8,
+		to: 11,
+		color: 'rgba(68, 170, 213, 0.1)',
+		label: {
+		    text: 'Too Hot',
+		    style: {
+			color: '#606060'
+		    }
+		}
+	    }]
+	},
+	tooltip: {
+	    valueSuffix: ' °F'
+	},
+	plotOptions: {
+	    spline: {
+		lineWidth: 4,
+		states: {
+		    hover: {
+			lineWidth: 5
+		    }
+		},
+		marker: {
+		    enabled: false
+		},
+		pointInterval: 3600000, // one hour
+		pointStart: Date.UTC(2015, 4, 31, 0, 0, 0)
+	    }
+	},
+	series: [{
+	    name: 'Temperature',
+	    data: [0.2, 0.8, 0.8, 0.8, 1, 1.3, 1.5, 2.9, 1.9, 2.6, 1.6, 3, 4, 3.6, 4.5, 4.2, 4.5, 4.5, 4, 3.1, 2.7, 4, 2.7, 2.3, 2.3, 4.1, 7.7, 7.1, 5.6, 6.1, 5.8, 8.6, 7.2, 9, 10.9, 11.5, 11.6, 11.1, 12, 12.3, 10.7, 9.4, 9.8, 9.6, 9.8, 9.5, 8.5, 7.4, 7.6]
+
+	}, {
+	    name: 'Humidity',
+	    data: [0, 0, 0.6, 0.9, 0.8, 0.2, 0, 0, 0, 0.1, 0.6, 0.7, 0.8, 0.6, 0.2, 0, 0.1, 0.3, 0.3, 0, 0.1, 0, 0, 0, 0.2, 0.1, 0, 0.3, 0, 0.1, 0.2, 0.1, 0.3, 0.3, 0, 3.1, 3.1, 2.5, 1.5, 1.9, 2.1, 1, 2.3, 1.9, 1.2, 0.7, 1.3, 0.4, 0.3]
+	}],
+	navigation: {
+	    menuItemStyle: {
+		fontSize: '10px'
+	    }
+	}
+    });
+});
+
+
