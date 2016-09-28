2c7a0ffa5c9920a3dd2712d93877c0e976eb9797
Aaron Bieber <aaron@bolddaemon.com>
2016-09-28 16:05:55 -0600

    hightchargs

--- public/charts.js
+++ public/charts.js
@@ -45,76 +45,79 @@ function makePlotRange(min, max, count) {
 
 function makeSpeed(title, min, max, count) {
     return {
-	    chart: {
-		type: 'gauge',
-		plotBackgroundColor: null,
-		plotBackgroundImage: null,
-		plotBorderWidth: 0,
-		plotShadow: false
-	    },
+	chart: {
+	    type: 'gauge',
+	    plotBackgroundColor: null,
+	    plotBackgroundImage: null,
+	    plotBorderWidth: 0,
+	    plotShadow: false
+	},
 
+	title: {
+	    text: title
+	},
+	credits: {
+            enabled: false
+	},
+
+	pane: {
+	    startAngle: -150,
+	    endAngle: 150,
+	    background: [{
+		backgroundColor: {
+		    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+		    stops: [
+			[0, '#FFF'],
+			[1, '#333']
+		    ]
+		},
+		borderWidth: 0,
+		outerRadius: '109%'
+	    }, {
+		backgroundColor: {
+		    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+		    stops: [
+			[0, '#333'],
+			[1, '#FFF']
+		    ]
+		},
+		borderWidth: 1,
+		outerRadius: '107%'
+	    }, {
+		// default background
+	    }, {
+		backgroundColor: '#DDD',
+		borderWidth: 0,
+		outerRadius: '105%',
+		innerRadius: '103%'
+	    }]
+	},
+
+	// the value axis
+	yAxis: {
+	    min: 20,
+	    max: 120,
+
+	    minorTickInterval: 'auto',
+	    minorTickWidth: 1,
+	    minorTickLength: 10,
+	    minorTickPosition: 'inside',
+	    minorTickColor: '#666',
+
+	    tickPixelInterval: 30,
+	    tickWidth: 2,
+	    tickPosition: 'inside',
+	    tickLength: 10,
+	    tickColor: '#666',
+	    labels: {
+		step: 2,
+		rotation: 'auto'
+	    },
 	    title: {
-		text: title
+		text: 'F°'
 	    },
-
-	    pane: {
-		startAngle: -150,
-		endAngle: 150,
-		background: [{
-		    backgroundColor: {
-			linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-			stops: [
-			    [0, '#FFF'],
-			    [1, '#333']
-			]
-		    },
-		    borderWidth: 0,
-		    outerRadius: '109%'
-		}, {
-		    backgroundColor: {
-			linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-			stops: [
-			    [0, '#333'],
-			    [1, '#FFF']
-			]
-		    },
-		    borderWidth: 1,
-		    outerRadius: '107%'
-		}, {
-		    // default background
-		}, {
-		    backgroundColor: '#DDD',
-		    borderWidth: 0,
-		    outerRadius: '105%',
-		    innerRadius: '103%'
-		}]
-	    },
-
-	    // the value axis
-	    yAxis: {
-		min: 20,
-		max: 120,
-
-		minorTickInterval: 'auto',
-		minorTickWidth: 1,
-		minorTickLength: 10,
-		minorTickPosition: 'inside',
-		minorTickColor: '#666',
-
-		tickPixelInterval: 30,
-		tickWidth: 2,
-		tickPosition: 'inside',
-		tickLength: 10,
-		tickColor: '#666',
-		labels: {
-		    step: 2,
-		    rotation: 'auto'
-		},
-		title: {
-		    text: 'F°'
-		},
-		plotBands: makePlotRange(min, max, count)
-	    },
+	    plotBands: makePlotRange(min, max, count)
+	},
 
 	series: [{
 	    name: title,
@@ -235,6 +238,9 @@ $(function () {
 	},
 	tooltip: {
 	    valueSuffix: ' °F'
+	},
+	credits: {
+            enabled: false
 	},
 	plotOptions: {
 	    spline: {
