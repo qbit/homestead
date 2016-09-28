fe6124547b58ad13ca58f4d482cf0b8657598382
Aaron Bieber <aaron@bolddaemon.com>
2016-09-28 14:08:03 -0600

    new charts for low and high values

--- public/charts.js
+++ public/charts.js
@@ -261,38 +261,145 @@ $(function () {
 });
 
 var lsMap = {
-"temp": "Temperature",
-"humidity": "Humidity"
+    "temp": "Temperature",
+    "humidity": "Humidity"
 };
 
 function lineSeries(data, map) {
+    var o = {}, d, i, len = data.length, k, count = 0;
+    for (i = 0; i < len; i++) {
+	d = JSON.parse(data[i]);
+	if (count === 0) {
+	    for (k in d) {
+		if (lsMap[k]) {
+		    o[k] = {};
+		    o[k].data = [];
+		    o[k].name = lsMap[k];
+		}
+	    }
+	}
 
-var o = {}, d, i, len = data.length, k, count = 0;
-for (i = 0; i < len; i++) {
-d = JSON.parse(data[i]);
-if (count === 0) {
-for (k in d) {
-if (lsMap[k]) {
-o[k] = {};
-o[k].data = [];
-o[k].name = lsMap[k];
-}
-}
+	for (k in d) {
+	    if (lsMap[k]) {
+		o[k].data.push(d[k]);
+	    }
+	}
+
+	count++;
+    }
+
+    var r = [];
+    for (k in o) {
+	r.push(o[k]);
+    }
+
+    return r;
 }
 
-for (k in d) {
-if (lsMap[k]) {
-o[k].data.push(d[k]);
-}
-}
+var gaugeOptions = {
 
-count++;
-}
+    chart: {
+        type: 'solidgauge'
+    },
 
-var r = [];
-for (k in o) {
-r.push(o[k]);
-}
+    title: null,
 
-return r;
-}
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+};
+
+$('#high-temp').highcharts(Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: -100,
+        max: 200,
+        title: {
+            text: 'High Temp'
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'High Temp',
+        data: [80],
+        dataLabels: {
+            format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                '<span style="font-size:12px;color:silver">°F</span></div>'
+        },
+        tooltip: {
+            valueSuffix: ' °F'
+        }
+    }]
+
+}));
+
+// The RPM gauge
+$('#low-temp').highcharts(Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: -100,
+        max: 200,
+        title: {
+            text: 'Low Temp'
+        }
+    },
+
+    series: [{
+        name: 'Low Temp',
+        data: [1],
+        dataLabels: {
+            format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y:.1f}</span><br/>' +
+                '<span style="font-size:12px;color:silver">°F</span></div>'
+        },
+        tooltip: {
+            valueSuffix: '°F'
+        }
+    }]
+
+}));
+
